f935d68699a3e03dd1ee8d4a71c86bc3812778a5
Rajkumar <rk235216.rk@gmail.com>
2020-01-26 08:50:07 +0530

    headings changed, new pages included

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,29 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/income-tax', function () {
+    return view('incometax');
+});
+Route::get('/gst', function () {
+    return view('gst');
+});
+Route::get('/tds', function () {
+    return view('tds');
+});
+Route::get('/company', function () {
+    return view('company');
+});
+Route::get('/accounting', function () {
+    return view('accounting');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/import-export', function () {
+    return view('import_export');
+});
+Route::get('/property-single', function () {
+    return view('property-single');
 });
